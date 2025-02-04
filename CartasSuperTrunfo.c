69ecd3a058cacc8fb8f403f63cedb3189b3c958e
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-02-04 01:17:59 +0000

    nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 #define NUM_ESTADOS 8
 #define CIDADES_POR_ESTADO 4
 
+// Estrutura para armazenar os dados das cidades
 typedef struct {
     char codigo[4];
     int populacao;
@@ -11,8 +13,10 @@ typedef struct {
     int pontos_turisticos;
     float densidade_populacional;
     float pib_per_capita;
+    float super_poder;
 } Cidade;
 
+// Função para cadastrar os dados das cidades
 void cadastrarCartas(Cidade cartas[NUM_ESTADOS * CIDADES_POR_ESTADO]) {
     char estados[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
     int index = 0;
@@ -25,9 +29,9 @@ void cadastrarCartas(Cidade cartas[NUM_ESTADOS * CIDADES_POR_ESTADO]) {
             scanf("%d", &cartas[index].populacao);
             printf("Area (km^2): ");
             scanf("%f", &cartas[index].area);
-            printf("PIB (milhões): ");
+            printf("PIB (milhoes): ");
             scanf("%f", &cartas[index].pib);
-            printf("Número de pontos turísticos: ");
+            printf("Numero de pontos turisticos: ");
             scanf("%d", &cartas[index].pontos_turisticos);
             
             // Cálculo das propriedades adicionais
@@ -43,27 +47,56 @@ void cadastrarCartas(Cidade cartas[NUM_ESTADOS * CIDADES_POR_ESTADO]) {
                 cartas[index].pib_per_capita = 0;
             }
             
+            // Cálculo do Super Poder
+            cartas[index].super_poder = cartas[index].populacao + cartas[index].area + cartas[index].pib + cartas[index].pontos_turisticos;
+            
             index++;
         }
     }
 }
 
+// Função para exibir os dados das cidades
 void exibirCartas(Cidade cartas[NUM_ESTADOS * CIDADES_POR_ESTADO]) {
     printf("\nCartas cadastradas:\n");
     for (int i = 0; i < NUM_ESTADOS * CIDADES_POR_ESTADO; i++) {
-        printf("\nCódigo: %s\n", cartas[i].codigo);
-        printf("População: %d\n", cartas[i].populacao);
-        printf("Área: %.2f km^2\n", cartas[i].area);
-        printf("PIB: %.2f milhões\n", cartas[i].pib);
-        printf("Pontos turísticos: %d\n", cartas[i].pontos_turisticos);
+        printf("\nCodigo: %s\n", cartas[i].codigo);
+        printf("Populacao: %d\n", cartas[i].populacao);
+        printf("Area: %.2f km^2\n", cartas[i].area);
+        printf("PIB: %.2f milhoes\n", cartas[i].pib);
+        printf("Pontos turisticos: %d\n", cartas[i].pontos_turisticos);
         printf("Densidade Populacional: %.2f hab/km^2\n", cartas[i].densidade_populacional);
-        printf("PIB per Capita: %.2f milhões/hab\n", cartas[i].pib_per_capita);
+        printf("PIB per Capita: %.2f milhoes/hab\n", cartas[i].pib_per_capita);
+        printf("Super Poder: %.2f\n", cartas[i].super_poder);
     }
+}
+
+// Função para comparar duas cartas
+void compararCartas(Cidade c1, Cidade c2) {
+    printf("\nComparacao entre %s e %s:\n", c1.codigo, c2.codigo);
+    printf("Populacao: %s\n", (c1.populacao > c2.populacao) ? c1.codigo : c2.codigo);
+    printf("Area: %s\n", (c1.area > c2.area) ? c1.codigo : c2.codigo);
+    printf("PIB: %s\n", (c1.pib > c2.pib) ? c1.codigo : c2.codigo);
+    printf("Pontos turisticos: %s\n", (c1.pontos_turisticos > c2.pontos_turisticos) ? c1.codigo : c2.codigo);
+    printf("Densidade Populacional: %s (menor vence)\n", (c1.densidade_populacional < c2.densidade_populacional) ? c1.codigo : c2.codigo);
+    printf("PIB per Capita: %s\n", (c1.pib_per_capita > c2.pib_per_capita) ? c1.codigo : c2.codigo);
+    printf("Super Poder: %s\n", (c1.super_poder > c2.super_poder) ? c1.codigo : c2.codigo);
 }
 
 int main() {
     Cidade cartas[NUM_ESTADOS * CIDADES_POR_ESTADO];
     cadastrarCartas(cartas);
     exibirCartas(cartas);
+    
+    // Comparação entre duas cartas
+    int c1, c2;
+    printf("\nEscolha duas cartas para comparar (0 a %d): ", NUM_ESTADOS * CIDADES_POR_ESTADO - 1);
+    scanf("%d %d", &c1, &c2);
+    
+    if (c1 >= 0 && c1 < NUM_ESTADOS * CIDADES_POR_ESTADO && c2 >= 0 && c2 < NUM_ESTADOS * CIDADES_POR_ESTADO) {
+        compararCartas(cartas[c1], cartas[c2]);
+    } else {
+        printf("Indices invalidos!\n");
+    }
+    
     return 0;
 }
