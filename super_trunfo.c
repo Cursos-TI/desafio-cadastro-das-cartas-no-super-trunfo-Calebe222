8ca899395f4afa3709101d6faebfedbe8f77b29c
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-02-19 01:57:27 +0000

    finalização da primeira parte - desafio novato

--- super_trunfo.c
+++ super_trunfo.c
@@ -28,7 +28,7 @@ int main () {
     printf("Código da carta: (ex: A01): "); 
     scanf("%s", codigo1);
     printf("Nome da Cidade: "); 
-    scanf("%s\n", cidade1);
+    scanf(" %[^\n]", cidade1);
     printf("População: ");
     scanf("%d", &populacao1);
     printf("Area (Km²): ");
@@ -39,13 +39,40 @@ int main () {
     scanf("%d", &pontos_turisticos1);
 
     // Entrada de dados da segunda carta
-    printf("Digite os dados da segunda carta: ");
-    printf("Estados (A-H): "); 
-    scanf("%c", &estado2); 
-    printf("Código da carta: (ex: B02): ");
-    
+    printf("\nDigite os dados da segunda carta:\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+    printf("Código da Carta (ex: B02): ");
+    scanf("%s", codigo2);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", cidade2);
+    printf("População: ");
+    scanf("%d", &populacao2);
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+    printf("PIB (em bilhões): ");
+    scanf("%f", &pib2);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos2);
 
+    // Exibição das informações das cartas
+    printf("\n=== Carta 1 ===\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
 
+    printf("\n=== Carta 1 ===\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
-
+    return 0; 
 }
